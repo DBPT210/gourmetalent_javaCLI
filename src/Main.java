53271f3c41d7b6fb30d04f2a9347317ce0ca574a
diogo <diogobeja04@gmail.com>
2024-01-23 13:15:40 +0000

    Adicionado o principio do menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Isto e o trabalho");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("'gourmet|alent'");
+        System.out.println("");
+        System.out.println("O que você vai querer cozinhar hoje?");
+        System.out.println("Por favor, selecione por número.");
+        System.out.println("1. Sopas e Cozidos;");
+        System.out.println("2. Carne");
+        System.out.println("3. Peixe;");
+        System.out.println("4. Sobremesas;");
     }
 }
